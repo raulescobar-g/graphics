612902ec76a219f54a8ecd648bb84899fceee77b
Raul Escobar <74686538+raulescobar-g@users.noreply.github.com>
2022-08-20 20:01:00 -0500

    Delete zProj directory

--- zProj/README.md
+++
@@ -1,7 +0,0 @@
-# A5
-
-Raul Escobar
-raulescobar_g@tamu.edu
-328003859
-
-completed \_\_\_ tasks
--- zProj/src/Material.cpp
+++
@@ -1,4 +0,0 @@
-#include "Material.h"
-
-
-Material::Material(glm::vec3 ke, glm::vec3 kd, glm::vec3 ks, float s): ke(ke), kd(kd), ks(ks), s(s) {}
